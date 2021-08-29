4e441f3be785896fd861c369eb69e62db4081dc8
lisa <batareikaenergia@mail.ru>
2021-08-29 03:19:25 +0300

    добавлено решение ДЗ-3

---
+++ homework_3/task_3.py
@@ -0,0 +1,12 @@
+# Реализовать функцию my_func(), которая принимает три позиционных аргумента, и возвращает сумму наибольших двух аргументов.
+
+def my_func (num1, num2, num3):
+    nums_sum= num1 + num2 + num3
+    return nums_sum - min((num1, num2, num3))
+
+num1 = int(input('Введите первое число: '))
+num2 = int(input('Введите второе число: '))
+num3 = int(input('Введите третье число: '))
+result = my_func(num1, num2, num3)
+
+print(result)
