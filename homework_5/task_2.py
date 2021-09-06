c4b8f5ea8f1ca044e040cfac6bd7663fde5bfc83
lisa <batareikaenergia@mail.ru>
2021-09-07 01:25:32 +0300

    обновлено 2 задание в ДЗ-5

--- homework_5/task_2.py
+++ homework_5/task_2.py
@@ -1,7 +1,8 @@
 # 2. Создать текстовый файл (не программно), сохранить в нем несколько строк, выполнить
 # подсчет количества строк, количества слов в каждой строке.
+import codecs
 
-with open('text.txt') as file:
+with codecs.open('text.txt', encoding="UTF-8") as file:
     file_lini1 = file.readlines()
     for num, line in enumerate(file_lini1):
         word_count = len(line.split())
